6f06cd5a237c069f6b41915be24be0262db131ec
Ashwin Madhu <ashwin.madhu@experionglobal.local>
2022-02-03 08:11:12 +0530

    Shared folder and bootstrap

--- ClinicManagementSystem/src/app/app.module.ts
+++ ClinicManagementSystem/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentListComponent
   ],
   imports: [
     BrowserModule,
