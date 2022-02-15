d3601f4986a680f00a745ab224b2e6cc31a7cb47
arathiexperion <96334141+arathiexperion@users.noreply.github.com>
2022-02-15 10:23:32 +0530

    Delete ClinicManagementSystem directory

--- ClinicManagementSystem/src/app/shared/receptionist.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Receptionist } from './receptionist';
-
-describe('Receptionist', () => {
-  it('should create an instance', () => {
-    expect(new Receptionist()).toBeTruthy();
-  });
-});
